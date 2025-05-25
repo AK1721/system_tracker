9d6128c43cec80e76d233b90b0d1670cf79e2361
Adham Khalifa <adhamkhalifa5000@gmail.com>
2025-05-25 12:08:44 +0300

    update collectors scripts

--- collectors/processes.sh
+++ collectors/processes.sh
@@ -4,4 +4,3 @@ get_running_processes(){
     ps -eo pid,ppid,comm,%mem,%cpu,start_time
 }
 
-get_running_processes
--- main.sh
+++
@@ -1,9 +0,0 @@
-source ./collectors/users.sh
-
-
-
-get_num_of_users
-echo "----------------------------------"
-get_list_of_normal_users
-echo "----------------------------------"
-get_list_of_service_users
